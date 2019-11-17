f8aeb655c850c114ca39eec6dbb2c6be970d0141
Somar <somar.jaafar19@gmail.com>
2019-11-16 21:54:42 -0500

    123_Added_EditorSnap

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+
+    [Range(1f,20f)] [SerializeField] float gridSize =10f;
+
+    void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = 0f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPos;
+
+    }
+}
+
+
